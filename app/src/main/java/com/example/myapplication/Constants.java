95880af907071c9a51b0d492ac2cfe923150fb28
jit <abedulrahman2017@gmail.com>
2019-04-21 14:35:55 +0300

    ok

--- app/src/main/java/com/example/myapplication/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.example.myapplication;
-
-public interface Constants {
-    public static final int MESSAGE_STATE_CHANGE=1;
-    public static final int MESSAGE_READ=2;
-}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,23 +1,13 @@
 package com.example.myapplication;
 
-
-import android.app.TabActivity;
-import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TabHost;
 
-
-public class MainActivity extends TabActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-TabHost tabHost=getTabHost();
-tabHost.addTab(tabHost.newTabSpec("main").setIndicator("main").setContent(new Intent(this,Main2Activity.class)));
-        tabHost.addTab(tabHost.newTabSpec("main").setIndicator("do sub").setContent(new Intent(this,Main2Activity.class)));
-
-tabHost.setCurrentTab(0);
-
+        setContentView(R.layout.activity_main);
     }
 }
